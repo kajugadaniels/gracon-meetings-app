a3e8921fac3da84db5f99938f7d799e8639d3e24
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-18 10:42:48 +0200

    chore(ui): export shared primitives

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Shared UI barrel for app/meetings.
+ */
+export { Button } from './Button';
+export { Input } from './Input';
+export { Card } from './Card';
+export { PremiumLoader } from './Loader';
