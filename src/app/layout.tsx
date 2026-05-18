ab5f4eed4786d0e538ba62c2777430e3f18f3f12
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-18 14:28:35 +0200

    fix(meetings): align font variable with documents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { DM_Sans } from 'next/font/google';
 import './globals.css';
 
 const dmSans = DM_Sans({
-    variable: '--font-sans',
+    variable: '--font-dm-sans',
     subsets: ['latin'],
 });
 
